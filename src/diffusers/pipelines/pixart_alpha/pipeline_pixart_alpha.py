5df38e74a1813d481e392a76e11f5d3d34249d99
Aivean <aiveeen@gmail.com>
2024-05-04 15:10:55 -0700

    fix calling sites of PixArtImageProcessor.resize_and_crop_tensor

--- src/diffusers/pipelines/pixart_alpha/pipeline_pixart_alpha.py
+++ src/diffusers/pipelines/pixart_alpha/pipeline_pixart_alpha.py
@@ -930,7 +930,7 @@ class PixArtAlphaPipeline(DiffusionPipeline):
         if not output_type == "latent":
             image = self.vae.decode(latents / self.vae.config.scaling_factor, return_dict=False)[0]
             if use_resolution_binning:
-                image = self.image_processor.resize_and_crop_tensor(image, orig_width, orig_height)
+                image = self.PixArtImageProcessor.resize_and_crop_tensor(image, orig_width, orig_height)
         else:
             image = latents
 
